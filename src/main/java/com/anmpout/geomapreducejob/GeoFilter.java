27625fe061037e0de04613756e6e5bb36ed487c2
cloudera <cloudera@quickstart.cloudera>
2019-04-02 08:43:05 -0700

    add params to the jar exec

--- src/main/java/com/anmpout/geomapreducejob/GeoFilter.java
+++ src/main/java/com/anmpout/geomapreducejob/GeoFilter.java
@@ -39,55 +39,12 @@ public class GeoFilter extends Configured implements Tool {
 
     private static final Logger LOG = Logger.getLogger(GeoFilter.class);
 
-    @Override
-    public int run(String[] args) throws Exception {
-        String outputPath = "/user/thesis/sample1/output/"
-                + Long.toString(Calendar.getInstance().getTimeInMillis());
-
-        Job job = Job.getInstance(getConf(), "GeoFilter");
-        job.setJarByClass(this.getClass());
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream("/home/cloudera/Downloads/mapperoutput.txt"), "utf-8"))) {
-            writer.write(outputPath);
-        }
-        FileInputFormat.addInputPath(job, new Path("/user/thesis/samples/fcd_gps_02_2018"));
-        FileOutputFormat.setOutputPath(job, new Path(outputPath));
-        // FileInputFormat.addInputPath(job, new Path(args[0]));
-        // FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.setMapperClass(GeoFilterMapper.class);
-        job.setMapOutputKeyClass(GeoKeyMap.class);
-        job.setMapOutputValueClass(GeoValue.class);
-        job.setInputFormatClass(GeoInputFormat.class);
-        job.setReducerClass(GeoFilterReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-
-        if (job.waitForCompletion(true) && job.isSuccessful()) {
-            Configuration hadoopConfig = new Configuration();
-            hadoopConfig.set("fs.defaultFS", "hdfs://quickstart.cloudera:8020");
-            hadoopConfig.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-            FileSystem fs = FileSystem.get(hadoopConfig);
-            Path inputPath2 = new Path(outputPath.trim().replace("\n", "") + "/part-r-00000");
-            openFileFromHDFS(fs, inputPath2);
-
-        }
-
-        return job.waitForCompletion(true) ? 0 : 1;
-
-    }
-
-    public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new GeoFilter(), args);
-        System.exit(exitCode);
-
-    }
-
-    private static void saveItemToDB(String readLine, Connection conn) {
+    private static void saveItemToDBRealTime(String readLine, Connection conn) {
         try {
             // the mysql insert statement
-            String query = " insert into FILTER_DATA (PATH_ID,TIMESTAMP ,COUNT, SPEED, TIME)"
-                    + " values (?, ?, ?, ?, ?)";
+            String query = " insert into FILTER_DATA_RT (PATH_ID,TIMESTAMP ,COUNT, SPEED,"
+                    + " TIME, MAX_SPEED, MIN_SPEED, MEDIAN_SPEED, DAY, MONTH, YEAR)"
+                    + " values (?, ?, ?, ?, ?,?, ?, ?, ?,?,?)";
 
             // create the mysql insert preparedstatement
             String[] parts = readLine.split("\t");
@@ -122,6 +79,179 @@ public class GeoFilter extends Configured implements Tool {
             } else {
                 preparedStmt.setInt(5, 0);
             }
+                        //MAX         
+            if (!parts[6].equals("") && !parts[6].equals("NaN")) {
+                preparedStmt.setDouble(6, Double.parseDouble(parts[6]));
+            } else {
+                preparedStmt.setDouble(6, 0);
+            }
+                                    //min         
+            if (!parts[7].equals("") && !parts[7].equals("NaN")) {
+                preparedStmt.setDouble(7, Double.parseDouble(parts[7]));
+            } else {
+                preparedStmt.setDouble(7, 0);
+            }
+             //median         
+            if (!parts[8].equals("") && !parts[8].equals("NaN")) {
+                preparedStmt.setDouble(8, Double.parseDouble(parts[8]));
+            } else {
+                preparedStmt.setDouble(8, 0);
+            }
+                        //day         
+            if (!parts[9].equals("") && !parts[9].equals("NaN")) {
+                preparedStmt.setInt(9, Integer.parseInt(parts[9]));
+            } else {
+                preparedStmt.setInt(9, 0);
+            }
+                                    //month         
+            if (!parts[10].equals("") && !parts[10].equals("NaN")) {
+                preparedStmt.setInt(10, Integer.parseInt(parts[10]));
+            } else {
+                preparedStmt.setInt(10, 0);
+            }
+                                                //year         
+            if (!parts[11].equals("") && !parts[11].equals("NaN")) {
+                preparedStmt.setInt(11, Integer.parseInt(parts[11]));
+            } else {
+                preparedStmt.setInt(11, 0);
+            }
+//            //DISTANCE
+//            if (!parts[5].equals("") && !parts[5].equals("NaN")) {
+//                preparedStmt.setDouble(6, Double.parseDouble(parts[5]));
+//            } else {
+//                preparedStmt.setDouble(6, 0.0);
+ //           }
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+
+        }    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        String outputPath = "/user/thesis/sample1/output/"
+                + Long.toString(Calendar.getInstance().getTimeInMillis());
+
+        Job job = Job.getInstance(getConf(), "GeoFilter");
+        job.setJarByClass(this.getClass());
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("/home/cloudera/Downloads/mapperoutput.txt"), "utf-8"))) {
+            writer.write(outputPath);
+        }
+        boolean realTime = Boolean.parseBoolean(args[1]);
+        boolean saveStats = Boolean.parseBoolean(args[2]);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+        // FileInputFormat.addInputPath(job, new Path(args[0]));
+        // FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setMapperClass(GeoFilterMapper.class);
+        job.setMapOutputKeyClass(GeoKeyMap.class);
+        job.setMapOutputValueClass(GeoValue.class);
+        job.setInputFormatClass(GeoInputFormat.class);
+        job.setReducerClass(GeoFilterReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        if (job.waitForCompletion(true) && job.isSuccessful() && saveStats) {
+            Configuration hadoopConfig = new Configuration();
+            hadoopConfig.set("fs.defaultFS", "hdfs://quickstart.cloudera:8020");
+            hadoopConfig.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+            FileSystem fs = FileSystem.get(hadoopConfig);
+            Path inputPath2 = new Path(outputPath.trim().replace("\n", "") + "/part-r-00000");
+            openFileFromHDFS(fs, inputPath2,realTime);
+
+        }
+
+        return job.waitForCompletion(true) ? 0 : 1;
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new GeoFilter(), args);
+        System.exit(exitCode);
+
+    }
+
+    private static void saveItemToDB(String readLine, Connection conn) {
+        try {
+            // the mysql insert statement
+            String query = " insert into FILTER_DATA (PATH_ID,TIMESTAMP ,COUNT, SPEED,"
+                    + " TIME, MAX_SPEED, MIN_SPEED, MEDIAN_SPEED, DAY, MONTH, YEAR)"
+                    + " values (?, ?, ?, ?, ?,?, ?, ?, ?,?,?)";
+
+            // create the mysql insert preparedstatement
+            String[] parts = readLine.split("\t");
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            //PATH_ID
+            if (!parts[0].equals("") && !parts[0].equals("NaN")) {
+                preparedStmt.setInt(1, Integer.parseInt(parts[0]));
+            } else {
+                preparedStmt.setInt(1, 0);
+            }
+            //TIMESTAMP
+            if (!parts[1].equals("") && !parts[1].equals("NaN")) {
+                preparedStmt.setLong(2, Long.parseLong(parts[1]));
+            } else {
+                preparedStmt.setLong(2, 0);
+            }
+            //COUNT
+            if (!parts[2].equals("") && !parts[2].equals("NaN")) {
+                preparedStmt.setInt(3, Integer.parseInt(parts[2]));
+            } else {
+                preparedStmt.setInt(3, 0);
+            }
+            //SPEED
+            if (!parts[3].equals("") && !parts[3].equals("NaN")) {
+                preparedStmt.setDouble(4, Double.parseDouble(parts[3]));
+            } else {
+                preparedStmt.setDouble(4, 0);
+            }
+            //TIME         
+            if (!parts[4].equals("") && !parts[4].equals("NaN")) {
+                preparedStmt.setInt(5, Integer.parseInt(parts[4]));
+            } else {
+                preparedStmt.setInt(5, 0);
+            }
+                        //MAX         
+            if (!parts[6].equals("") && !parts[6].equals("NaN")) {
+                preparedStmt.setDouble(6, Double.parseDouble(parts[6]));
+            } else {
+                preparedStmt.setDouble(6, 0);
+            }
+                                    //min         
+            if (!parts[7].equals("") && !parts[7].equals("NaN")) {
+                preparedStmt.setDouble(7, Double.parseDouble(parts[7]));
+            } else {
+                preparedStmt.setDouble(7, 0);
+            }
+             //median         
+            if (!parts[8].equals("") && !parts[8].equals("NaN")) {
+                preparedStmt.setDouble(8, Double.parseDouble(parts[8]));
+            } else {
+                preparedStmt.setDouble(8, 0);
+            }
+                        //day         
+            if (!parts[9].equals("") && !parts[9].equals("NaN")) {
+                preparedStmt.setInt(9, Integer.parseInt(parts[9]));
+            } else {
+                preparedStmt.setInt(9, 0);
+            }
+                                    //month         
+            if (!parts[10].equals("") && !parts[10].equals("NaN")) {
+                preparedStmt.setInt(10, Integer.parseInt(parts[10]));
+            } else {
+                preparedStmt.setInt(10, 0);
+            }
+                                                //year         
+            if (!parts[11].equals("") && !parts[11].equals("NaN")) {
+                preparedStmt.setInt(11, Integer.parseInt(parts[11]));
+            } else {
+                preparedStmt.setInt(11, 0);
+            }
 //            //DISTANCE
 //            if (!parts[5].equals("") && !parts[5].equals("NaN")) {
 //                preparedStmt.setDouble(6, Double.parseDouble(parts[5]));
@@ -139,7 +269,7 @@ public class GeoFilter extends Configured implements Tool {
 
     }
 
-    public static void openFileFromHDFS(FileSystem fs, Path path) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+    public static void openFileFromHDFS(FileSystem fs, Path path,boolean realTime) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
         InputStream is = fs.open(path);
 //        String url = "jdbc:mysql://127.0.0.1:3306/GeoSpatialDB";
 //        String user = "cloudera";
@@ -156,11 +286,21 @@ Connection conn=DriverManager.getConnection(
       
         String readLine;
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        if(realTime){
+                while (((readLine = br.readLine()) != null)) {
+             // String[] parts = readLine.split("\t");
+            saveItemToDBRealTime(readLine,conn);
+            System.out.println(readLine);
 
+        }    
+            
+        }else{
         while (((readLine = br.readLine()) != null)) {
+             // String[] parts = readLine.split("\t");
             saveItemToDB(readLine,conn);
             System.out.println(readLine);
 
+        }
         }
         conn.close();
     }
--- src/main/java/com/anmpout/geomapreducejob/GeoFilterReducer.java
+++ src/main/java/com/anmpout/geomapreducejob/GeoFilterReducer.java
@@ -6,6 +6,12 @@
 package com.anmpout.geomapreducejob;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -23,32 +29,75 @@ GeoValue, Text, Text> {
         int count = 0;
         int time = 0;
         String timeString ="";
+        List<Double> allSpeeds = new ArrayList<>();
         
       
        for (GeoValue value : values) {
            if(value.getSpeed().get()>0){
         speed = value.getSpeed().get() + speed;
         count = count+1;
+        allSpeeds.add(value.getSpeed().get());
            }
       }
+        Double max = max(allSpeeds);
+        Double min = min(allSpeeds);
+        Double median = getMedian(allSpeeds);
+        
+        
+       
        speed= speed/count;
        if(speed != 0){
        time = (int) (keyMap.getKey().getDistance().get()/speed);
        }
-      double hours = time / 3600;
-      double minutes = (time % 3600) / 60;
-      double seconds = time % 60;
-
+//      double hours = time / 3600;
+//      double minutes = (time % 3600) / 60;
+//      double seconds = time % 60;
+     long timestamp = keyMap.getTimeslot().get()*1000;
+    Date d = new Date(timestamp);
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(d);
+    int year = cal.get(Calendar.YEAR);
+    int month = cal.get(Calendar.MONTH)+1;
+    int day = cal.get(Calendar.DAY_OF_MONTH);
+        
 //timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
 
      context.write(keyMap.getKey().getLocation(),new Text(String.valueOf(keyMap.getTimeslot())+"\t"+String.valueOf(count)+"\t"+
-             String.valueOf(speed)+"\t"+String.valueOf(time)+"\t"+String.valueOf(keyMap.getKey().getDistance().get())));
+             String.valueOf(speed)+"\t"+String.valueOf(time)+"\t"+String.valueOf(keyMap.getKey().getDistance().get())+"\t"+
+             String.valueOf(max)+"\t"+String.valueOf(min)+"\t"+String.valueOf(median)+"\t"+
+             String.valueOf(day)+"\t"+String.valueOf(month)+"\t"+String.valueOf(year)));
 //      context.write(key.getLocation(), new Text());
 //       context.write(key.getLocation(), new Text()); 
 //       context.write(key.getLocation(), new Text()+"\n\n"));  
     }
     
-    
-    
-    
+       public Double min(List<Double> array) {
+     if(array.isEmpty()) return 0.0;
+      Double min = array.get(0);
+     
+      for(int i=0; i<array.size(); i++) {
+         if(array.get(i).compareTo(min)<0) {
+            min = array.get(i);
+         }
+      }
+      return min;
+   }
+    public Double getMedian(List<Double> array) {
+    if(array.isEmpty()) return  0.0;
+    Collections.sort(array);
+    int middle = array.size() / 2;
+    middle = middle > 0 && middle % 2 == 0 ? middle - 1 : middle;
+    return array.get(middle);
+}
+      public Double max(List<Double> array) {
+      if(array.isEmpty()) return 0.0;
+      Double max = array.get(0);
+     
+      for(int i=0; i<array.size(); i++) {
+         if(array.get(i).compareTo(max)>0) {
+            max = array.get(i);
+         }
+      }
+      return max;
+   }
 }
